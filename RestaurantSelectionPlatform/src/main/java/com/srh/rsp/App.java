8989325a8dcaa94f5c290585c68d10f0ce7fc070
Rohit Dipankar <rohitdipankar@gmail.com>
2018-12-09 17:45:04 +0100

    Update App.java

--- RestaurantSelectionPlatform/src/main/java/com/srh/rsp/App.java
+++ RestaurantSelectionPlatform/src/main/java/com/srh/rsp/App.java
@@ -1,11 +1,14 @@
 package com.srh.rsp;
 
+import com.srh.rsp.Restaurant.CusineDishes;
+
+import distanceAlgorithm.CalDistance;
+
 public class App {
 
 	public static void main(String[] args) {
 
 		LoginHome mainPage = new LoginHome();
 		mainPage.ConsoleMenu();
-
 	}
 }
