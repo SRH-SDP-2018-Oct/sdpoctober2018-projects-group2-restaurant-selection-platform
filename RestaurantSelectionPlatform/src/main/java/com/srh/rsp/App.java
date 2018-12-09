4d41ef86bf293d40f26ae22eb8a38323369e02fc
MANU SRIVASTAVA <sayhello2manu@gmail.com>
2018-12-09 17:31:23 +0100

    jaspwe update

--- RestaurantSelectionPlatform/src/main/java/com/srh/rsp/App.java
+++ RestaurantSelectionPlatform/src/main/java/com/srh/rsp/App.java
@@ -3,7 +3,8 @@ package com.srh.rsp;
 public class App {
 
 	public static void main(String[] args) {
-		
-		 LoginHome mainPage = new LoginHome(); mainPage.ConsoleMenu();
+
+		LoginHome mainPage = new LoginHome();
+		mainPage.ConsoleMenu();
 	}
 }
