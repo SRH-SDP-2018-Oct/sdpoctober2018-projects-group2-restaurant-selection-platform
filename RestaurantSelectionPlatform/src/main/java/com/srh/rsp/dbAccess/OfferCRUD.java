c86e9afdde52a365d4894bf5aa11367ce3d607a8
Abhiodinson <shivakumarabhishek94@gmail.com>
2018-12-12 13:50:21 +0100

    Notification changes

--- RestaurantSelectionPlatform/src/main/java/com/srh/rsp/dbAccess/OfferCRUD.java
+++ RestaurantSelectionPlatform/src/main/java/com/srh/rsp/dbAccess/OfferCRUD.java
@@ -1,14 +1,22 @@
 package com.srh.rsp.dbAccess;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import com.srh.rsp.PersistenceManager;
 import com.srh.rsp.entity.OfferDetails;
+
 //
 public class OfferCRUD {
 	long restaurantid, dishid;
 	float offerpercentage;
 	String offerdescription;
+	EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+	CriteriaBuilder cbuilder = PersistenceManager.INSTANCE.getCriteriaBuilder();
 
 	public void setNewOffer(long restaurantid, long dishid, float offerpercentage, String offerdescription) {
 
@@ -25,4 +33,17 @@ public class OfferCRUD {
 		em.close();
 	}
 
+	public List<OfferDetails> offerdetailsonRestaurantId(Long restaurantid) {
+		CriteriaQuery<OfferDetails> criteriaQuery = cbuilder.createQuery(OfferDetails.class);
+		Root<OfferDetails> offerdetailsRoot = criteriaQuery.from(OfferDetails.class);
+		criteriaQuery.select(offerdetailsRoot);
+		criteriaQuery.where(cbuilder.equal(offerdetailsRoot.get("RestaurantId"), restaurantid));
+		List<OfferDetails> listofofferdetailsDetails = em.createQuery(criteriaQuery).getResultList();
+		if (listofofferdetailsDetails.isEmpty()) {
+			// list is empty
+			return null;
+		}
+		return listofofferdetailsDetails;
+
+	}
 }
